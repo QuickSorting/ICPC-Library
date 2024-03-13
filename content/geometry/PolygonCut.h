0e87c5961957c6d1392ae394cfa65c48b3096520
Viktor Kozhuharov <kozhuharov.viktor@gmail.com>
2024-03-13 16:06:12 +0200

    Add reminder to put EPS if dealing with floating point values

--- content/geometry/PolygonCut.h
+++ content/geometry/PolygonCut.h
@@ -27,8 +27,8 @@ vector<P> polygonCut(const vector<P>& poly, P s, P e) {
 	vector<P> res;
 	rep(i,0,sz(poly)) {
 		P cur = poly[i], prev = i ? poly[i-1] : poly.back();
-		bool side = s.cross(e, cur) < 0;
-		if (side != (s.cross(e, prev) < 0))
+		bool side = s.cross(e, cur) < 0; // < EPS if double
+		if (side != (s.cross(e, prev) < 0)) // < EPS if double
 			res.push_back(lineInter(s, e, cur, prev).second);
 		if (side)
 			res.push_back(cur);
